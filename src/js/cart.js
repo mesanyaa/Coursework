bbb7908ab25634237e496949822968d7e6b64ffe
mesanyaa <iamsashayakovchuk@gmail.com>
2023-12-05 21:31:44 +0300

    cart

--- src/js/cart.js
+++ src/js/cart.js
@@ -3,43 +3,43 @@ let carts = document.querySelectorAll(".add-cart");
 let products = [
   // product 1
   {
-    name: "Summer LilacBush",
+    name: "Летняя сирень",
     tag: "summerlilacbush",
     price: 4999,
     inCart: 0,
   },
   // product 2
   {
-    name: "The courage of a gentleman",
+    name: "Мужество Джентельмена",
     tag: "thecourageofagentleman",
     price: 4999,
     inCart: 0,
   },
   // product 3
   {
-    name: "Magic of the East",
+    name: "Магия Востока",
     tag: "magicoftheeast",
     price: 4999,
     inCart: 0,
   },
   // product 4
   {
-    name: "Spring Inspiration",
+    name: "Весеннее вдохновение",
     tag: "springinspiration",
     price: 4999,
     inCart: 0,
   },
   // product 5
   {
-    name: "Fruit Harmony",
+    name: "Фруктовая Гармония",
     tag: "fruitharmony",
     price: 4999,
     inCart: 0,
   },
   // product 6
   {
-    name: "Wood Energy",
-    tag: "wood energy",
+    name: "Древесная энергия",
+    tag: "woodenergy",
     price: 4999,
     inCart: 0,
   },
@@ -135,4 +135,174 @@ function totalCost(product) {
   }
 }
 
+function deleteItem(keyToRemove) {
+  // Получаем массив из локального хранилища
+  let storedArray = JSON.parse(localStorage.getItem("productsInCart")) || [];
+
+  // Проверяем, существует ли ключ в объекте
+  if (storedArray.hasOwnProperty(keyToRemove)) {
+    let cartCost = localStorage.getItem("totalCost");
+    cartCost = parseInt(cartCost, 10);
+    cartCost -=
+      storedArray[keyToRemove].price * storedArray[keyToRemove].inCart;
+    localStorage.setItem("totalCost", cartCost);
+
+    let productNumbers = localStorage.getItem("cartNumbers");
+    productNumbers = parseInt(productNumbers, 10);
+    productNumbers -= storedArray[keyToRemove].inCart;
+    localStorage.setItem("cartNumbers", productNumbers);
+
+    // Удаляем элемент из объекта
+    delete storedArray[keyToRemove];
+
+    // Обновляем локальное хранилище с обновленным объектом
+    localStorage.setItem("productsInCart", JSON.stringify(storedArray));
+    console.log("running");
+  } else {
+    console.log("Элемент не найден в объекте.");
+  }
+}
+
+function increaseProduct(keyToRemove) {
+  // Получаем массив из локального хранилища
+  let storedArray = JSON.parse(localStorage.getItem("productsInCart")) || [];
+
+  // Проверяем, существует ли ключ в объекте
+  if (storedArray.hasOwnProperty(keyToRemove)) {
+    let cartCost = localStorage.getItem("totalCost");
+    cartCost = parseInt(cartCost, 10);
+    cartCost += storedArray[keyToRemove].price;
+    localStorage.setItem("totalCost", cartCost);
+
+    let productNumbers = localStorage.getItem("cartNumbers");
+    productNumbers = parseInt(productNumbers, 10);
+    productNumbers += 1;
+    localStorage.setItem("cartNumbers", productNumbers);
+    storedArray[keyToRemove].inCart += 1;
+    localStorage.setItem("productsInCart", JSON.stringify(storedArray));
+
+    let elemQuantity = document.querySelector(`.quantity_${keyToRemove}`);
+    elemQuantity.textContent = storedArray[keyToRemove].inCart;
+
+    let elemTotal = document.querySelector(`.total_${keyToRemove}`);
+    elemTotal.textContent = `${
+      storedArray[keyToRemove].inCart * storedArray[keyToRemove].price
+    } руб.`;
+  } else {
+    console.log("Элемент не найден в объекте.");
+  }
+}
+
+function decreaseProduct(keyToRemove) {
+  let elemQuantity = document.querySelector(`.quantity_${keyToRemove}`);
+  if (parseInt(elemQuantity.textContent, 10) > 1) {
+    // Получаем массив из локального хранилища
+    let storedArray = JSON.parse(localStorage.getItem("productsInCart")) || [];
+
+    // Проверяем, существует ли ключ в объекте
+    if (storedArray.hasOwnProperty(keyToRemove)) {
+      let cartCost = localStorage.getItem("totalCost");
+      cartCost = parseInt(cartCost, 10);
+      cartCost -= storedArray[keyToRemove].price;
+      localStorage.setItem("totalCost", cartCost);
+
+      let productNumbers = localStorage.getItem("cartNumbers");
+      productNumbers = parseInt(productNumbers, 10);
+      productNumbers -= 1;
+      localStorage.setItem("cartNumbers", productNumbers);
+      storedArray[keyToRemove].inCart -= 1;
+      localStorage.setItem("productsInCart", JSON.stringify(storedArray));
+
+      let elemQuantity = document.querySelector(`.quantity_${keyToRemove}`);
+      elemQuantity.textContent = storedArray[keyToRemove].inCart;
+
+      let elemTotal = document.querySelector(`.total_${keyToRemove}`);
+      elemTotal.textContent = `${
+        storedArray[keyToRemove].inCart * storedArray[keyToRemove].price
+      } руб.`;
+    } else {
+      console.log("Элемент не найден в объекте.");
+    }
+  }
+}
+
+function displayCart() {
+  let cartItems = localStorage.getItem("productsInCart");
+  cartItems = JSON.parse(cartItems);
+
+  let productContainer = document.querySelector(".products-container");
+  let cartCost = localStorage.getItem("totalCost");
+
+  let productNumbers = localStorage.getItem("cartNumbers");
+  if (productNumbers) {
+    document.querySelector(".products-section__number  span").textContent =
+      productNumbers;
+    document.querySelector(".products-section__total span").textContent =
+      cartCost;
+  }
+  console.log(cartItems);
+  if (cartItems && productContainer) {
+    productContainer.innerHTML = "";
+    Object.values(cartItems).map((item) => {
+      productContainer.innerHTML += `
+      <div class="product">
+        <img src="./img/${item.tag}.jpg">
+        <span class="product-name">${item.name}</span>
+        <div class="quantity">
+          <div class="decrease-product ${
+            item.tag
+          }" style="background:transparent; border:none;">
+            <ion-icon name="remove-circle-outline"></ion-icon>
+          </div>
+          <span class="quantity_${item.tag}">${item.inCart}</span>
+          <div class="increase-product ${
+            item.tag
+          }" style="background:transparent; border:none;">
+          <ion-icon name="add-circle-outline"></ion-icon>
+          </div>
+        </div>
+        <button class="delete-product ${
+          item.tag
+        }" style="background:transparent; border:none;">
+          <ion-icon name="trash-outline"></ion-icon>
+        </button>
+        <div class="price price_${item.tag}">${item.price} руб.</div>
+        <div class="total total_${item.tag}">${
+        item.inCart * item.price
+      } руб.</div>
+      </div>
+      <div class="products-section__line"></div>
+      `;
+    });
+    let deleteButtons = document.querySelectorAll(".delete-product");
+    for (let i = 0; i < deleteButtons.length; i++) {
+      deleteButtons[i].addEventListener("click", () => {
+        let buttonClasses = deleteButtons[i].classList;
+        let classArray = Array.from(buttonClasses);
+        deleteItem(classArray[1]);
+        location.reload();
+      });
+    }
+    let increaseButtons = document.querySelectorAll(".increase-product");
+    for (let i = 0; i < increaseButtons.length; i++) {
+      increaseButtons[i].addEventListener("click", () => {
+        let buttonClasses = increaseButtons[i].classList;
+        let classArray = Array.from(buttonClasses);
+        increaseProduct(classArray[1]);
+        location.reload();
+      });
+    }
+    let decreaseButtons = document.querySelectorAll(".decrease-product");
+    for (let i = 0; i < decreaseButtons.length; i++) {
+      decreaseButtons[i].addEventListener("click", () => {
+        let buttonClasses = decreaseButtons[i].classList;
+        let classArray = Array.from(buttonClasses);
+        decreaseProduct(classArray[1]);
+        location.reload();
+      });
+    }
+  }
+}
+
 /* onLoadCartNumbers(); */
+displayCart();
